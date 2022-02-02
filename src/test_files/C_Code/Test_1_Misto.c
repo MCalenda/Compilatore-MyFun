5d2950094b5a9954a683866d8edc37fbaaf6c763
MarcoCalenda14 <marco.calenda14@gmail.com>
2022-02-02 18:39:53 +0100

    task windows per compilare ed eseguire (installare gcc)

--- src/test_files/C_Code/Test_1_Misto.c
+++ src/test_files/C_Code/Test_1_Misto.c
@@ -1,32 +1,30 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <math.h>
 
 // Funzioni di conversione
-char *int_to_string(int x) {
+char *int_to_string(int x)
+{
     char *str = malloc(512 * sizeof(char));
     sprintf(str, "%d", x);
     return str;
 }
-char *double_to_string(double x) {
+char *double_to_string(double x)
+{
     char *str = malloc(512 * sizeof(char));
     sprintf(str, "%f", x);
     return str;
 }
 
-// Dichiarazione delle funzioni
-int sommac(int a, int b) { return a + b; }
-
 // Funzione Main
-int main() {
-    char *str = malloc(512 * sizeof(char));
-    strcpy(str, "");
-    int x;
-    strcat(str, str);
-    strcat(str, sommac(5, 4));
-    x = sommac(5, 4);
-    printf("%s", str);
+int main()
+{
+    int x = 5;
+    int y = 10;
+    int z = x + y;
+    printf("%d", z);
     printf("\r\n");
     return 0;
 }
