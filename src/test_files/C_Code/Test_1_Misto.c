2f021c9cb6d6182a6e899b762f94ccfc65cc1c08
MarcoCalenda14 <marco.calenda14@gmail.com>
2022-02-03 18:20:15 +0100

    aggiunta funzioni per concat

--- src/test_files/C_Code/Test_1_Misto.c
+++ src/test_files/C_Code/Test_1_Misto.c
@@ -4,27 +4,65 @@
 #include <string.h>
 #include <math.h>
 
-// Funzioni di conversione
-char *int_to_string(int x)
+// Funzioni di concatenazione
+char *concatInt(char *string, int toConcat)
 {
-    char *str = malloc(512 * sizeof(char));
-    sprintf(str, "%d", x);
-    return str;
+    int length = snprintf(NULL, 0, "%d", toConcat);
+    char *converted = (char *)malloc(length + 1);
+    sprintf(converted, "%d", toConcat);
+    char *concat = (char *)malloc(1 + strlen(string) + strlen(converted));
+    strcpy(concat, string);
+    strcat(concat, converted);
+    return concat;
 }
-char *double_to_string(double x)
+char *concatReal(char *string, float toConcat)
 {
-    char *str = malloc(512 * sizeof(char));
-    sprintf(str, "%f", x);
-    return str;
+    int length = snprintf(NULL, 0, "%f", toConcat);
+    char *converted = (char *)malloc(length + 1);
+    sprintf(converted, "%f", toConcat);
+    char *concat = (char *)malloc(1 + strlen(string) + strlen(converted));
+    strcpy(concat, string);
+    strcat(concat, converted);
+    return concat;
+}
+char *concatBool(char *string, int toConcat)
+{
+    char *converted = (char *)malloc(6);
+    if (toConcat == 1)
+        strcpy(converted, "true");
+    else
+        strcpy(converted, "false");
+    char *concat = (char *)malloc(1 + strlen(string) + strlen(converted));
+    strcpy(concat, string);
+    strcat(concat, converted);
+    return concat;
+}
+char *concatString(char *string, char *toConcat)
+{
+    char *concat = (char *)malloc(1 + strlen(string) + strlen(toConcat));
+    strcpy(concat, string);
+    strcat(concat, toConcat);
+    return concat;
+}
+
+// Dichiarazione delle funzioni
+void prova(char **a, int *b, int c, char *d)
+{
+    *a = concatString(*a, " e' cambiato");
+    *b = 6;
+    c = 7;
+    d = concatString(d, " o forse si");
 }
 
 // Funzione Main
 int main()
 {
-    int x = 5;
-    int y = 10;
-    int z = x + y;
-    printf("%d", z);
+    char *a = "questa";
+    int b = 5;
+    int c = 6;
+    char *d = "codesta non e' cambiata";
+    prova(&a, &b, c, d);
+    printf("%s", concatString(concatString(concatInt(concatString(concatInt(concatString(concatString(a, " "), " "), b), " "), c), " "), d));
     printf("\r\n");
     return 0;
 }
