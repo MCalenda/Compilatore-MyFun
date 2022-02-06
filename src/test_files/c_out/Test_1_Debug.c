ec6e97666ffcbaaa421fceb7c99b48bce5971f1f
GabrielePisapia <50744224+GabrielePisapia@users.noreply.github.com>
2022-02-06 11:51:53 +0100

    Fix strcmp per !=

--- src/test_files/c_out/Test_1_Debug.c
+++ src/test_files/c_out/Test_1_Debug.c
@@ -1,11 +1,12 @@
-#include <math.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <math.h>
 
 // Funzioni di concatenazione
-char *concatInt(char *string, int toConcat) {
+char *concatInt(char *string, int toConcat)
+{
     int length = snprintf(NULL, 0, "%d", toConcat);
     char *converted = (char *)malloc(length + 1);
     sprintf(converted, "%d", toConcat);
@@ -14,7 +15,8 @@ char *concatInt(char *string, int toConcat) {
     strcat(concat, converted);
     return concat;
 }
-char *concatReal(char *string, float toConcat) {
+char *concatReal(char *string, float toConcat)
+{
     int length = snprintf(NULL, 0, "%f", toConcat);
     char *converted = (char *)malloc(length + 1);
     sprintf(converted, "%f", toConcat);
@@ -23,7 +25,8 @@ char *concatReal(char *string, float toConcat) {
     strcat(concat, converted);
     return concat;
 }
-char *concatBool(char *string, int toConcat) {
+char *concatBool(char *string, int toConcat)
+{
     char *converted = (char *)malloc(6);
     if (toConcat == 1)
         strcpy(converted, "true");
@@ -34,7 +37,8 @@ char *concatBool(char *string, int toConcat) {
     strcat(concat, converted);
     return concat;
 }
-char *concatString(char *string, char *toConcat) {
+char *concatString(char *string, char *toConcat)
+{
     char *concat = (char *)malloc(1 + strlen(string) + strlen(toConcat));
     strcpy(concat, string);
     strcat(concat, toConcat);
@@ -42,11 +46,14 @@ char *concatString(char *string, char *toConcat) {
 }
 
 // Funzione Main
-int main() {
+int main()
+{
     int choose = 0;
     bool conti = true;
-    while (conti) {
-        if (choose == 0) {
+    while (conti)
+    {
+        if (choose == 0)
+        {
             conti = false;
         }
     }
